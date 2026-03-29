de35616e23a616656061a16edc586dab7b9b0057
Yogesh Makawana <yogesh.makwana@gojek.com>
2026-03-30 00:14:37 +0530

    - remove bundle id

--- Sources/KeyboardKit/Host/KeyboardInputViewController+Host.swift
+++ Sources/KeyboardKit/Host/KeyboardInputViewController+Host.swift
@@ -22,8 +22,7 @@ public extension KeyboardInputViewController {
     /// if this feature suddenly stops working.
     var hostApplicationBundleId: String? {
         if Bundle.main.isExtension {
-            if let id = hostBundleIdValueBefore16 { return id }
-            return hostBundleIdValueFor16
+           return nil
         } else {
             return Bundle.main.bundleIdentifier
         }
@@ -43,27 +42,7 @@ private extension KeyboardInputViewController {
     }
     
     var hostBundleIdValueFor16: String? {
-        guard let pid = parent?.value(forKey: "_hostPID") else { return nil }
-        let selector = NSSelectorFromString("defaultService")
-        guard let anyClass: AnyObject = NSClassFromString("PKService"),
-           let pkService = anyClass as? NSObjectProtocol,
-           pkService.responds(to: selector),
-           let serverInis = pkService.perform(selector).takeUnretainedValue() as? NSObjectProtocol
-        else { return nil }
-        let lities = serverInis.perform(NSSelectorFromString("personalities")).takeUnretainedValue()
-        let bundleId = Bundle.main.bundleIdentifier ?? ""
-        guard let infos = lities.object(forKey: bundleId) as? AnyObject,
-           let info = infos.object(forKey: pid) as? AnyObject,
-           let con = info.perform(NSSelectorFromString("connection")).takeUnretainedValue() as? NSObjectProtocol
-        else { return nil }
-        let xpcCon = con.perform(NSSelectorFromString("_xpcConnection")).takeUnretainedValue()
-        let handle = dlopen("/usr/lib/libc.dylib", RTLD_NOW)
-        let sym = dlsym(handle, "xpc_connection_copy_bundle_id")
-        typealias xpcFunc = @convention(c) (AnyObject) -> UnsafePointer<CChar>
-        let cFunc = unsafeBitCast(sym, to: xpcFunc.self)
-        let response = cFunc(xpcCon)
-        let hostBundleId = NSString(utf8String: response)
-        return hostBundleId as String?
+        return nil
     }
 }
 #endif
